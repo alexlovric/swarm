ffa89abaf0fc68625e48c56de2cdf1c957b20b7d
Aleksander Lovric <alexlovric@gmail.com>
2025-07-09 17:12:46 +0200

    removed version from swarm_py

--- swarm_py/tests/test_import.py
+++ swarm_py/tests/test_import.py
@@ -1,6 +1,11 @@
+"""
+This is a basic import test for swarm_py. 
+Used to verify that the package is installed correctly on the various operating systems.
+"""
+
 import swarm_py as sp
 
-print(f"Successfully imported swarm version: {sp.__version__}")
+# Create variable basic build test
 var = sp.Variable(0, 1)
 print(f"Successfully created a swarm.Variable object: {var}")
 print("Basic import test passed!")
